3a5f1501882cbadc1c0ee7fccce9c41b6c10253f
Laurrini <154939183+Laurrini@users.noreply.github.com>
2024-03-04 21:51:50 -0600

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
-function getCity(event) {
+function refreshWeather(response) {
+  let temperatureElement = response.data.temperature.current;
+  let weatherAppValueElement = document.querySelector("#weather-value");
+  let temperature = Math.round(temperatureElement);
+  let city = document.querySelector("#weather-app-city");
+  let descriptionElement = document.querySelector("#weather-condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  weatherAppValueElement.innerHTML = temperature;
+  city.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/hr`;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
+  let apiKey = "aae4390da28do47e0e9dfc3fdaftcdbb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let city = document.querySelector("#weather-app-city");
-  city.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFomrElement = document.querySelector("#search-form");
-searchFomrElement.addEventListener("submit", getCity);
+searchFomrElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
